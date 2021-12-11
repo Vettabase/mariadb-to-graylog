47679929fa0dd43b38386aa0ba49538958fb3ef7
Federico Razzoli <federico.razzoli@vettabase.com>
2021-12-11 17:58:33 +0000

    Document global constants

--- registry.py
+++ registry.py
@@ -15,10 +15,14 @@ class Registry:
     ##  Global Constants
     ##  ================
 
+    # Program name
     PROGRAM = 'MariaDB To Graylog'
+    # Program version
     VERSION = '0.1'
+    # Program description
     DESCRIPTION = 'Consume MariaDB error & slow logs and send them to GrayLog'
 
+    # Backup flags, for additional output
     DEBUG = {
         # Print GELF messages before sending them
         'GELF_MESSAGES': True,
