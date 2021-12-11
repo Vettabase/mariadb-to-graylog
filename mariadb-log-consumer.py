d301d8277484aef776b481fd1cb6ace87db3ad2c
Federico Razzoli <federico.razzoli@vettabase.com>
2021-12-11 17:49:51 +0000

    Move registry into a separate module

--- mariadb-log-consumer.py
+++ mariadb-log-consumer.py
@@ -5,28 +5,7 @@ import sys
 import signal
 
 from lib_consumer import *
-
-
-class Registry:
-    """
-    Global constants (Registry pattern)
-    """
-
-    PROGRAM = 'MariaDB To Graylog'
-    VERSION = '0.1'
-    DESCRIPTION = 'Consume MariaDB error & slow logs and send them to GrayLog'
-
-    DEBUG = {
-        # Print GELF messages before sending them
-        'GELF_MESSAGES': True,
-        # Print read log lines
-        'LOG_LINES': False,
-        # Print info about parsed log lines
-        'LOG_PARSER': False,
-
-        # Consumer instance
-        "consumer": None
-    }
+from registry import Registry
 
 
 class Consumer:
---
+++ registry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+
+class Registry:
+    """
+    Global constants (Registry pattern)
+    """
+
+    PROGRAM = 'MariaDB To Graylog'
+    VERSION = '0.1'
+    DESCRIPTION = 'Consume MariaDB error & slow logs and send them to GrayLog'
+
+    DEBUG = {
+        # Print GELF messages before sending them
+        'GELF_MESSAGES': True,
+        # Print read log lines
+        'LOG_LINES': False,
+        # Print info about parsed log lines
+        'LOG_PARSER': False,
+
+        # Consumer instance
+        "consumer": None
+    }
