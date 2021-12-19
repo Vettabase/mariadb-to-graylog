9f2cd4b8c3c5497b455412d4d82e06042acb662e
Federico Razzoli <federico.razzoli@vettabase.com>
2021-12-19 16:33:52 +0000

    Request_Counters: Add docstrings to methods

--- lib_consumer/request_counters.py
+++ lib_consumer/request_counters.py
@@ -28,29 +28,36 @@ class Request_Counters:
     ##  =======
 
     def __init__(self, action_list):
+        """ Accept a list or tuple of actions, assign them to _request_counters. """
         for action in action_list:
             print(action)
             self._request_counters[action] = 0
 
     def increment(self, action):
+        """ Increment an action counter by 1. """
         try:
             self._request_counters[action] = self._request_counters[action] + 1
         except KeyError as e:
             raise Exception('[Request_Counters.increment] Action does not exist: \'' + action + '\'')
 
     def reset(self, action):
+        """ Reset an action counter to zero. """
         try:
             self._request_counters[action] = 0
         except KeyError as e:
             raise Exception('[Request_Counters.reset] Action does not exist: \'' + action + '\'')
 
     def get_action(self, action):
+        """ Return how many times an action was called since the beginning or
+            last reset.
+        """
         try:
             return self._request_counters[action]
         except KeyError as e:
             raise Exception('[Request_Counters.get_action] Action does not exist: \'' + action + '\'')
 
     def was_requested(self, action):
+        """ Return whether an action was called at least once or not. """
         try:
             return (self._request_counters[action] > 0)
         except KeyError as e:
