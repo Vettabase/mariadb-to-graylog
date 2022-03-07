b18ecf9b8dec6f0e9db4662f4ec9575bcd87dc2d
Federico Razzoli <federico.razzoli@vettabase.com>
2022-03-07 11:38:23 +0000

    Implement Graylog_Client_HTTP

--- lib_consumer/__init__.py
+++ lib_consumer/__init__.py
@@ -6,6 +6,7 @@ from gelf_message import GELF_Message
 from graylog_client import Graylog_Client
 from graylog_client_udp import Graylog_Client_UDP
 from graylog_client_tcp import Graylog_Client_TCP
+from graylog_client_http import Graylog_Client_HTTP
 from request_counters import Request_Counters
 
 #EOF
---
+++ lib_consumer/graylog_client_http.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+
+""" Send messages to Graylog using a TCP port.
+"""
+
+
+from graylog_client import Graylog_Client
+
+
+class Graylog_Client_HTTP(Graylog_Client):
+    """ Send messages to Graylog using a TCP port.
+    """
+
+
+    import requests
+    # Needed to convert JSON string to dictionary
+    import json
+
+
+    #: Graylog URL that will receive requests, including host and port.
+    _url = None
+    #: Socket used to connect Graylog.
+    _sock = None
+
+
+    def __init__(self, host, port=12201):
+        """ Compose Graylog URL. """
+        self._url = 'http://' + host + ':' + port + '/gelf'
+
+    def send(self, gelf_message):
+        """ Send the specified GELF message over an HTTP request. """
+        requests.get(self._url, data=json.loads(gelf_message))
+
+#EOF
