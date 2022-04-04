b6d91c0acf7d627065f6710c126026fbe70e33c5
Federico Razzoli <federico.razzoli@vettabase.com>
2022-04-04 01:27:43 +0100

    Use Requests native options for retry

--- lib_consumer/graylog_client_http.py
+++ lib_consumer/graylog_client_http.py
@@ -14,6 +14,8 @@ class Graylog_Client_HTTP(Graylog_Client):
 
 
     import requests
+    from requests.adapters import HTTPAdapter
+    from requests.packages.urllib3.util.retry import Retry
     # Needed to convert JSON string to dictionary
     import json
     # Used to implement timeouts
@@ -26,37 +28,43 @@ class Graylog_Client_HTTP(Graylog_Client):
     _graylog_http_timeout_idle = None
     #: HTTP requests timeout, hard limit.
     _graylog_http_timeout = None
+    #: HTTP connection configuration
+    _connection = None
 
 
     def __init__(self, host, port=12201, graylog_http_timeout_idle=None, graylog_http_timeout=None):
         """ Compose Graylog URL. """
         self._url = 'http://' + host + ':' + str(port) + '/gelf'
 
+        retry_strategy = self.Retry(
+            total=3,
+            backoff_factor=1
+        )
+        adapter = self.HTTPAdapter(max_retries=retry_strategy)
+        self._connection = self.requests.Session()
+        self._connection.mount('https://', adapter)
+        self._connection.mount('http://', adapter)
+
     def send(self, gelf_message):
         """ Send the specified GELF message over an HTTP request. """
         # Set a hard timeout for the HTTP call
         self.eventlet.monkey_patch()
         with self.eventlet.Timeout(self._graylog_http_timeout):
-            success = False
-            attempts_left = 3
-            while (not success) and attempts_left > 0:
-                attempts_left = attempts_left - 1
-                try:
-                    self.requests.post(
-                        self._url,
-                        headers={
-                            'Content-Type': 'application/json',
-                            'User-Agent': 'Vettabase/mariadb-to-graylog'
-                        },
-                        json=self.json.loads(gelf_message),
-                        timeout=self._graylog_http_timeout_idle,
-                        verify=True,
-                        allow_redirects=False
-                    )
-                    success = True
-                # requests exceptions are listed here:
-                # https://docs.python-requests.org/en/latest/user/quickstart/#errors-and-exceptions
-                except Exception as e:
-                    print(str(e))
+            try:
+                self._connection.post(
+                    self._url,
+                    headers={
+                        'Content-Type': 'application/json',
+                        'User-Agent': 'Vettabase/mariadb-to-graylog'
+                    },
+                    json=self.json.loads(gelf_message),
+                    timeout=self._graylog_http_timeout_idle,
+                    verify=True,
+                    allow_redirects=False
+                )
+            # requests exceptions are listed here:
+            # https://docs.python-requests.org/en/latest/user/quickstart/#errors-and-exceptions
+            except Exception as e:
+                print(str(e))
 
 #EOF
