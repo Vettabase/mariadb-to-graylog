cf0f3c5e8429d9788271e00810f4757dbba65415
Federico Razzoli <federico.razzoli@vettabase.com>
2022-04-18 03:03:31 +0100

    Complete type hinting for request_counters

--- lib_consumer/request_counters.py
+++ lib_consumer/request_counters.py
@@ -11,6 +11,9 @@
 """
 
 
+from typing import List
+
+
 class Request_Counters:
     """
         A dictionary of request counters, with the methods necessary to
@@ -21,32 +24,32 @@ class Request_Counters:
     ##  =========
 
     #: Dictionary of request counters.
-    _request_counters = { }
+    _request_counters: dict[str, int] = { }
 
 
     ##  Methods
     ##  =======
 
-    def __init__(self, action_list):
+    def __init__(self, action_list: list[str]):
         """ Accept a list or tuple of actions, assign them to _request_counters. """
         for action in action_list:
             self._request_counters[action] = 0
 
-    def increment(self, action):
+    def increment(self, action: str) -> None:
         """ Increment an action counter by 1. """
         try:
             self._request_counters[action] = self._request_counters[action] + 1
         except KeyError as e:
             raise Exception('[Request_Counters.increment] Action does not exist: \'' + action + '\'')
 
-    def reset(self, action):
+    def reset(self, action: str) -> None:
         """ Reset an action counter to zero. """
         try:
             self._request_counters[action] = 0
         except KeyError as e:
             raise Exception('[Request_Counters.reset] Action does not exist: \'' + action + '\'')
 
-    def get_action(self, action):
+    def get_action(self, action: str) -> int:
         """ Return how many times an action was called since the beginning or
             last reset.
         """
@@ -55,7 +58,7 @@ class Request_Counters:
         except KeyError as e:
             raise Exception('[Request_Counters.get_action] Action does not exist: \'' + action + '\'')
 
-    def was_requested(self, action):
+    def was_requested(self, action: str) -> bool:
         """ Return whether an action was called at least once or not. """
         try:
             return (self._request_counters[action] > 0)
