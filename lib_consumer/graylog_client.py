059b0e8e8f6b8086bb2ccfc4ae8e16ea08927ebb
Federico Razzoli <federico.razzoli@vettabase.com>
2022-04-18 13:18:35 +0100

    Complete type hinting for Graylog_Client

--- lib_consumer/graylog_client.py
+++ lib_consumer/graylog_client.py
@@ -6,16 +6,19 @@
 """
 
 
+from .gelf_message import GELF_Message
+
+
 class Graylog_Client:
     """ Send messages to Graylog.
     """
 
 
-    def __init__(self, host, port):
+    def __init__(self, host: str, port: str):
         """ Assign values to private members. """
         pass
 
-    def send(self, gelf_message):
+    def send(self, gelf_message: GELF_Message) -> None:
         """ Send the specified UDP packet. """
         pass
 
