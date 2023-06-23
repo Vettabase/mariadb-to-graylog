46ea46520d7c6eb91327a83b3d71bdf799a784de
Federico Razzoli <federico.razzoli@vettabase.com>
2023-06-23 06:57:03 +0100

    Add comments

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+
+# Install required Python modules
 pip -r requirements.txt
 
+# Download (replace) pt-fingerprint
 rm -f pt-fingerprint
 wget https://www.percona.com/get/pt-fingerprint
 chmod ugo+x pt-fingerprint
